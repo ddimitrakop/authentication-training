ca4d1a7306123bc161b09ca9798a0497420362f7
Dimitrios Dimitrakopoulos <ddimitrakop0@gmail.com>
2024-09-29 10:46:34 +0300

    initialized project

---
+++ src/main/java/com/ddimi/authentication_training/AuthenticationTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.ddimi.authentication_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddimi/authentication_training/AuthenticationTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddimi.authentication_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
